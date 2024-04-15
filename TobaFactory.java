d0d7ed138b6caa10dce797d709b76db55eb503e9
Theodor <111547477+theodorbb@users.noreply.github.com>
2024-04-15 21:07:42 +0300

    Delete TobaFactory.java

--- TobaFactory.java
+++
@@ -1,8 +0,0 @@
-package FactoryMethod.Clase;
-
-public class TobaFactory implements IFactory{
-    @Override
-    public AInstrumentMuzical creareInstrumentMuzical(String marca, String culoare, String material) {
-        return new Toba(marca, culoare, material, 0);
-    }
-}
