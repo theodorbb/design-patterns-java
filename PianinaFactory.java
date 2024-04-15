d4b5091f0b138bfd8555e3b5f279c7ada731c0d8
Theodor <111547477+theodorbb@users.noreply.github.com>
2024-04-15 21:07:21 +0300

    Delete PianinaFactory.java

--- PianinaFactory.java
+++
@@ -1,9 +0,0 @@
-package FactoryMethod.Clase;
-
-public class PianinaFactory implements IFactory{
-
-    @Override
-    public AInstrumentMuzical creareInstrumentMuzical(String marca, String culoare, String material) {
-        return new Pianina(marca,culoare,material,0);
-    }
-}
